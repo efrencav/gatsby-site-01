7077cb018710926043c966849e7457e94158e6dd
abstract <info@abstract-creative.com>
2019-11-01 13:10:04 -0500

    graphql

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -9,6 +9,7 @@ exports.components = {
   "component---src-pages-404-js": hot(preferDefault(require("/Users/ecavazos/Desktop/gatsby-site-01/src/pages/404.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/ecavazos/Desktop/gatsby-site-01/src/pages/index.js"))),
   "component---src-pages-info-about-js": hot(preferDefault(require("/Users/ecavazos/Desktop/gatsby-site-01/src/pages/info/about.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/ecavazos/Desktop/gatsby-site-01/src/pages/page-2.js")))
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/ecavazos/Desktop/gatsby-site-01/src/pages/page-2.js"))),
+  "component---src-pages-info-files-info-js": hot(preferDefault(require("/Users/ecavazos/Desktop/gatsby-site-01/src/pages/info/files-info.js")))
 }
 
