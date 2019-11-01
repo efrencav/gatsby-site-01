7a18a0734e193b58551124e058b9141b805bda99
abstract <info@abstract-creative.com>
2019-11-01 14:21:40 -0500

    graphql

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,9 +7,10 @@ const preferDefault = m => m && m.default || m
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/ecavazos/Desktop/gatsby-site-01/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/ecavazos/Desktop/gatsby-site-01/src/pages/404.js"))),
+  "component---src-pages-blog-index-js": hot(preferDefault(require("/Users/ecavazos/Desktop/gatsby-site-01/src/pages/blog/index.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/ecavazos/Desktop/gatsby-site-01/src/pages/index.js"))),
   "component---src-pages-info-about-js": hot(preferDefault(require("/Users/ecavazos/Desktop/gatsby-site-01/src/pages/info/about.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/ecavazos/Desktop/gatsby-site-01/src/pages/page-2.js"))),
-  "component---src-pages-info-files-info-js": hot(preferDefault(require("/Users/ecavazos/Desktop/gatsby-site-01/src/pages/info/files-info.js")))
+  "component---src-pages-info-files-info-js": hot(preferDefault(require("/Users/ecavazos/Desktop/gatsby-site-01/src/pages/info/files-info.js"))),
+  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/ecavazos/Desktop/gatsby-site-01/src/pages/page-2.js")))
 }
 
