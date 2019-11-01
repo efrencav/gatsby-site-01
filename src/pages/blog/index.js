3546a0ecf38ee9d886e936d5b6c4ba2cbcad7178
abstract <info@abstract-creative.com>
2019-11-01 16:13:50 -0500

    linked blog

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 import styled from'styled-components';
 import Layout from '../../components/layout2';
 
@@ -17,9 +17,11 @@ export default ({data}) => (
             {data.allMarkdownRemark.edges.map(({node}) => (
                 <div key={node.id}>
                     <hr/>
-                    <h3>{node.frontmatter.title}</h3>
-                    <p><i>{node.frontmatter.date}</i></p>
-                    <p>{node.excerpt}</p>
+                    <Link to={node.fields.slug} style={{textDecoration: 'none'}}>
+                        <h3>{node.frontmatter.title}</h3>
+                        <p><i>{node.frontmatter.date}</i></p>
+                        <p>{node.excerpt}</p>
+                    </Link>
                 </div>
             ))}
         </div>
@@ -38,6 +40,9 @@ export const query = graphql`
                         date(formatString: "DD MMMM, YYYY")
                     }
                     excerpt
+                    fields {
+                        slug
+                    }
                 }
             }
         }
